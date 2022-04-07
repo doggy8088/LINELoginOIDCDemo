632b9b042442853f148be89a95bf3f9c9e175b27
Will <doggy.huang@gmail.com>
2022-04-08 01:04:43 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LINELoginOIDCDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
